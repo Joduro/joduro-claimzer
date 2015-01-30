72b8a187269e4100f8d6b453b1e1dcd377fcc317
Joduro <joduro@ualberta.ca>
2015-01-29 20:21:54 -0700

    First Commit

---
+++ src/com/example/joduro_claimzer/Claim.java
@@ -0,0 +1,90 @@
+package com.example.joduro_claimzer;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Claim {
+
+	private String name;
+	private String desc;
+	private Date startDate;
+	private Date endDate;
+	private String status;
+	
+	private ArrayList<Expense> expensesList;
+	
+	public Claim(String name, String desc, Date startDate, Date endDate,
+			String status, ArrayList<Expense> expensesList) {
+		super();
+		this.name = name;
+		this.desc = desc;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.status = status;
+		this.expensesList = expensesList;
+	}
+
+	public void addExpense(Expense expense) {
+		for (int i = 0; i < expensesList.size();++i){
+			if(expense.getDate().before(expensesList.get(i).getDate())){
+				expensesList.add(i, expense);
+			}
+		}
+		expensesList.add(expense);
+	}
+	
+	public void removeExpense(Expense expense) {
+		expensesList.remove(expense);
+	}
+	
+	/*
+	public void loadClaim()
+	{
+		
+	}
+	
+	public void saveClaim()
+	{
+		
+	}
+	*/
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
---
+++ src/com/example/joduro_claimzer/Expense.java
@@ -0,0 +1,43 @@
+package com.example.joduro_claimzer;
+
+import java.util.Date;
+
+public class Expense {
+	
+	private Date date;
+	private String desc;
+	private String category;
+	private double cost;
+	private String currency;
+
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+}
